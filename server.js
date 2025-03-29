714cd46219c09b4d388ade39e22715bc21fce04f
NaN-codeZ <625muhammadfadhilah@gmail.com>
2025-03-29 11:39:36 +0700

    headless

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.post("/scrape", async (req, res) => {
     url = convertToReviewURL(url);
     console.log("🔗 Menggunakan URL:", url);
 
-    const browser = await puppeteer.launch({ headless: false, args: ["--no-sandbox", "--disable-setuid-sandbox"] });
+    const browser = await puppeteer.launch({ headless: "new", args: ["--no-sandbox", "--disable-setuid-sandbox"] });
     const page = await browser.newPage();
     await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
 
